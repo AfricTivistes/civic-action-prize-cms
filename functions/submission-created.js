f4d222e70eacb36c0370adbc12f3722d5d5df949
Mamadou Diagne <genova@dofbi.com>
2021-08-29 23:16:11 +0000

    👷 chore(form): submission

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,7 +1,6 @@
 const creatKey = require('./createKey')
 
 exports.handler = async (event, context, callback) => {
-
     const data = JSON.parse(event.body).payload
     await creatKey(data).then(
         result => {

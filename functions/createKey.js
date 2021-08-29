6c2517095ab50ff69ee0b961e5be8601ca3b9785
Mamadou Diagne <genova@dofbi.com>
2021-08-29 22:25:45 +0000

    ✨ feat(Vote): key verification

---
+++ functions/createKey.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+var jwt = require('jsonwebtoken')
+
+const { JWT_SECRET } = process.env
+
+const creatKey = async (data) => {
+
+    const { email, name, link, langue } = data.data
+    const user = {email: email, name:name, link,link ,langue: langue}
+    const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1h' })
+
+    return new Promise((resolve) => {
+
+        resolve("key", token)
+
+    })
+}
+
+module.exports = creatKey

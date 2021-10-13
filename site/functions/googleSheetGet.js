297c4c7063cf63ce3f47d7a3bb380fd8548c4f42
Mamadou Diagne <genova@dofbi.com>
2021-10-13 01:18:37 +0000

    ♻️ refactor(API): Access-Control-Allow-Origin

--- site/functions/googleSheetGet.js
+++ site/functions/googleSheetGet.js
@@ -1,27 +1,39 @@
 require("dotenv").config();
-const { GoogleSpreadsheet } = require("google-spreadsheet");
+const {GoogleSpreadsheet} = require("google-spreadsheet");
 
-const { GOOGLE_SERVICE_ACCOUNT_EMAIL, GOOGLE_PRIVATE_KEY, GOOGLE_SHEET } = process.env;
+const {GOOGLE_SERVICE_ACCOUNT_EMAIL, GOOGLE_PRIVATE_KEY, GOOGLE_SHEET} = process.env;
 
 const getvalue = async() => {
-  
-    const doc = new GoogleSpreadsheet(GOOGLE_SHEET);
-    await doc.useServiceAccountAuth({
-        client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: JSON.parse(GOOGLE_PRIVATE_KEY),
-    });
 
-    await doc.loadInfo(); // loads document properties and worksheets
+  const doc = new GoogleSpreadsheet(GOOGLE_SHEET);
+  await doc.useServiceAccountAuth({
+    client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: JSON.parse(GOOGLE_PRIVATE_KEY),
+  });
 
-    const sheet = doc.sheetsByIndex[1]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-    await sheet.loadCells('C2:C11');
-    const candidants = [];
-    for (let i = 1; i < 11; i++) {
-        candidants.push(sheet.getCell(i, 2).value);
-    }
+  await doc.loadInfo(); // loads document properties and worksheets
+
+  const sheet = doc.sheetsByIndex[1]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+  await sheet.loadCells('C2:C11');
+  const candidants = [];
+  for (let i = 1; i < 11; i++) {
+    candidants.push(sheet.getCell(i, 2).value);
+  }
 
   return new Promise((resolve) => {
-      resolve({ "c1": candidants[0], "c2": candidants[1], "c3": candidants[2], "c4": candidants[3], "c5": candidants[4], "c6": candidants[5], "c7": candidants[6], "c8": candidants[7], "c9": candidants[8], "c10": candidants[9], "c11": candidants[10] });
+    resolve([
+      {"id": "c1", "count": candidants[0]},
+      {"id": "c2", "count": candidants[1]},
+      {"id": "c3", "count": candidants[2]},
+      {"id": "c4", "count": candidants[3]},
+      {"id": "c5", "count": candidants[4]},
+      {"id": "c6", "count": candidants[5]},
+      {"id": "c7", "count": candidants[6]},
+      {"id": "c8", "count": candidants[7]},
+      {"id": "c9", "count": candidants[8]},
+      {"id": "c10", "count": candidants[9]},
+      {"id": "c11", "count": candidants[10]}
+    ]);
   });
 };
 

b672defb70f12d6b8d6d141f1bc4b96c46f39edc
Mamadou Diagne <genova@dofbi.com>
2021-11-11 15:37:16 +0000

    ♻️ refactor(Vote): thank message

--- functions/voteverify.js
+++ functions/voteverify.js
@@ -10,12 +10,14 @@ exports.handler = async(event, context, callback) => {
 
   try {
     const user = jwt.verify(key, JWT_SECRET);
+    const {langue} = data;
+    const link = (langue === "en") ? "https://prize.africtivistes.org/vote/?vote=yes" : "https://prize.africtivistes.org/fr/vote/?vote=yes";
     await addTosheet(user).then(
       (result) => {
         callback(null, {
           statusCode: 302,
           headers: {
-            "Location": `https://prize.africtivistes.org/vote/?vote=yes`
+            "Location": link
           },
         });
       });
@@ -23,7 +25,7 @@ exports.handler = async(event, context, callback) => {
     return {
       statusCode: 302,
       headers: {
-        "Location": `https://prize.africtivistes.org/`
+        "Location": "https://prize.africtivistes.org/"
       },
     };
   }
